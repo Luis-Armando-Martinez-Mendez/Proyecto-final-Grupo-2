52484970c2157a8c1eadbaf009a838c0d08b9c16
WilkerDev1 <capellancoronadowilker@gmail.com>
2025-12-10 21:06:16 -0400

    Implementación inicial del motor Monte Carlo con paralelismo
    Se crea la clase MonteCarloEngine e implementa el método EjecutarSimulacion.
    
    Uso de Parallel.For para la generación de trayectorias.
    
    Implementación de la fórmula de movimiento browniano geométrico.
    
    Recolección de resultados en estructura SimulationResults.

--- src/SimuladorMonteCarlo/SimuladorMonteCarlo/MonteCarloEngine.cs
+++ src/SimuladorMonteCarlo/SimuladorMonteCarlo/MonteCarloEngine.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
-namespace SimuladorMonteCarlo
+namespace MonteCarloSim
 {
-    internal class MonteCarloEngine
+    public class MonteCarloEngine
     {
+        // Método Paralelo Inicial
+        public SimulationResults EjecutarSimulacion(SimulationParameters param)
+        {
+            if (param.DiasTotales < 1) throw new Exception("Días insuficientes.");
+
+            double[][] resultados = new double[param.NumeroSimulaciones][];
+            Stopwatch sw = Stopwatch.StartNew();
+
+            var opcionesParalelas = new ParallelOptions { MaxDegreeOfParallelism = param.Hilos };
+
+            Parallel.For(0, param.NumeroSimulaciones, opcionesParalelas, i =>
+            {
+                Random azar = new Random(Guid.NewGuid().GetHashCode());
+                double[] trayectoria = new double[param.DiasTotales];
+                double precioActual = param.CapitalInicial;
+                trayectoria[0] = precioActual;
+
+                for (int dia = 1; dia < param.DiasTotales; dia++)
+                {
+                    double u1 = 1.0 - azar.NextDouble();
+                    double u2 = 1.0 - azar.NextDouble();
+                    double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2);
+
+                    double cambio = param.DerivaDiaria + (param.VolatilidadDiaria * randStdNormal);
+                    precioActual = precioActual * (1 + cambio);
+
+                    // NOTA: Aquí faltan las validaciones de límites
+
+                    trayectoria[dia] = precioActual;
+                }
+                resultados[i] = trayectoria;
+            });
+
+            sw.Stop();
+
+            double[] finales = resultados.Select(r => r[param.DiasTotales - 1]).ToArray();
+            Array.Sort(finales);
+
+            return new SimulationResults
+            {
+                Trayectorias = resultados,
+                TiempoEjecucionMs = sw.ElapsedMilliseconds,
+                HilosUsados = param.Hilos,
+                PreciosFinales = finales
+            };
+        }
     }
 }
