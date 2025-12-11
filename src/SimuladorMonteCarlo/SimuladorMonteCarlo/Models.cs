08a945a24bdf447f72eff793b333e63284a9dca4
Luis Armando Martinez Mendez <20240202@itla.edu.do>
2025-12-10 22:05:47 -0400

    incorporar modelos base para parámetros y resultados de la simulación
    Co-authored-by: <LuisMartinez>

--- src/SimuladorMonteCarlo/SimuladorMonteCarlo/Models.cs
+++ src/SimuladorMonteCarlo/SimuladorMonteCarlo/Models.cs
@@ -4,9 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimuladorMonteCarlo
+namespace MonteCarloSim
 {
-    internal class Models
+    public class SimulationParameters
     {
+        public double CapitalInicial { get; set; }
+        public int DiasTotales { get; set; }
+        public double VolatilidadDiaria { get; set; }
+        public double DerivaDiaria { get; set; }
+        public int NumeroSimulaciones { get; set; }
+        public double TasaLibreDiaria { get; set; }
+        public double TasaInflacionDiaria { get; set; }
+        public double InflacionInputTotal { get; set; } 
+        public int Hilos { get; set; }
+    }
+
+    
+    public class SimulationResults
+    {
+        public double[][] Trayectorias { get; set; } 
+        public long TiempoEjecucionMs { get; set; }
+        public int HilosUsados { get; set; }
+        public double[] PreciosFinales { get; set; } 
     }
 }
