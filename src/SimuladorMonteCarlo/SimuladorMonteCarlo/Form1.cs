48dcc1ad5746be3844ae1b2f064dbcc45fbe53c3
Luis Armando Martinez Mendez <20240202@itla.edu.do>
2025-12-10 22:47:32 -0400

    Agregar estructura base de Form1 e inicialización de los futuros servicios
    Co-authored-by: <LuisMartinez>

--- src/SimuladorMonteCarlo/SimuladorMonteCarlo/Form1.cs
+++ src/SimuladorMonteCarlo/SimuladorMonteCarlo/Form1.cs
@@ -1,10 +1,32 @@
-namespace SimuladorMonteCarlo
+using SimuladorMonteCarlo;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MonteCarloSim
 {
     public partial class Form1 : Form
     {
+        //private MonteCarloEngine engine = new MonteCarloEngine();
+        //private ChartService grafico = new ChartService();
+        //private ReportService reporte = new ReportService();
+
+        // Controles UI
+        private TextBox txtPrecio, txtPlazo, txtVolatilidad, txtDeriva, txtSimulaciones, txtTasaLibre, txtInflacion;
+        private ComboBox cmbUnidadTiempo, cmbUnidadVol, cmbUnidadDeriva;
+        private NumericUpDown numHilos;
+        private RichTextBox rtbResultados;
+        private Button btnSimular;
+
+        private Panel panelControles;
+        private TabControl tabGraficos;
+        private Chart chartLineas, chartHistograma, chartPastel, chartCaja;
+
         public Form1()
         {
             InitializeComponent();
+            ConstruirInterfaz();
         }
     }
 }
